57ea90105756de8e210ff3fab23733378c343908
xblax <xblax@incy.net>
2024-02-15 23:32:24 +0100

    install-script: display status on screen, improve error handling

--- device_files/install/flashforge_init.sh
+++ device_files/install/flashforge_init.sh
@@ -7,13 +7,22 @@ WORK_DIR="$(pwd)"
 MACHINE=$1
 PID=$2
 
+# error handling
+error_handler() {
+  xzcat $WORK_DIR/img/install_fail_error.img.xz > /dev/fb0
+  # always exit without error, to prevent immediate boot of flashforge software
+  exit 0
+}
+trap '[ $? -eq 0 ] && exit 0 || error_handler' EXIT
+
 # safety check, only install on supported printer
 if ([ "$MACHINE" != "Adventurer5M" ] && [ "$MACHINE" != "Adventurer5MPro" ]) || \
    ([ "$PID" != "0023" ] && [ "$PID" != "0024" ]) || \
     [ "$(uname -m)" != "armv7l" ]
  then
     echo "Update file does not match machine type."
-    exit 1
+    xzcat $WORK_DIR/img/install_fail_vers.img.xz > /dev/fb0
+    exit 0
 fi
 
 # saftey check, only install on supported versions
@@ -21,14 +30,15 @@ FF_VERSION="$(cat /root/version)"
 if [ "$FF_VERSION" != "2.4.5" ]
 then
     echo "Printer software version not supported."
-    exit 1
+    xzcat $WORK_DIR/img/install_fail_vers.img.xz > /dev/fb0
+    exit 0
 fi
 
 MOD_INIT_FILE="/etc/init.d/S90klipper_mod"
 MOD_DIR="/data/.klipper_mod"
 
 # update start image
-cat $WORK_DIR/start.img > /dev/fb0
+xzcat $WORK_DIR/img/install_start.img.xz > /dev/fb0
 # --------------------------------
 
 # uninstall previous mod version if present
@@ -41,7 +51,8 @@ MIN_SPACE="524228"
 if [ "$FREE_SPACE" -lt "$MIN_SPACE" ]
 then
     echo "Not enough free space on data partition. 512MB required!";
-    exit 1
+    xzcat $WORK_DIR/img/install_fail_mem.img.xz > /dev/fb0
+    exit 0
 fi
 
 # unpack chroot environment
@@ -51,16 +62,15 @@ xz -dc $WORK_DIR/chroot.tar.xz | tar -xf - -C $CHROOT_DIR
 sync
 
 # do intial setup
-$CHROOT_DIR/etc/S90klipper_mod setup
+$CHROOT_DIR/etc/init/S90klipper_mod setup
 sync
 
 # install initfile
-cp $CHROOT_DIR/etc/S90klipper_mod /etc/init.d/
+cp $CHROOT_DIR/etc/init/S90klipper_mod /etc/init.d/
 sync
 
 # update end image
 # --------------------------------
-cat $WORK_DIR/end.img > /dev/fb0
+xzcat $WORK_DIR/img/install_ok.img.xz > /dev/fb0
 
-$WORK_DIR/play
 exit 0

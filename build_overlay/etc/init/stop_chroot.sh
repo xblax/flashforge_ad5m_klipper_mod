f28b58603635e949b493050f58e0e241b3962c00
xblax <xblax@incy.net>
2024-03-09 16:22:10 +0100

    init/stop_chroot: reset hardware clock on shutdown/reboot
    Mainly to not confuse the host system with real time on warm reboot

--- build_overlay/etc/init/stop_chroot.sh
+++ build_overlay/etc/init/stop_chroot.sh
@@ -4,4 +4,9 @@
 /etc/init.d/rcK
 swapoff /mnt/swap
 
+# reset the hardware clock
+# this is mainly to not confuse the host system with real time on warm reboot
+date -s "1970-1-1 01:00:00"
+hwclock -w
+
 exit 0

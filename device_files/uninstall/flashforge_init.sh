da2bb6f78970df5e6812e2059c742da042570cfe
xblax <xblax@incy.net>
2024-03-10 02:30:01 +0100

    Add files for archive-based mod removal

---
+++ device_files/uninstall/flashforge_init.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -ex
+
+# workdir contains our update files
+cd "$(dirname "$0")"
+WORK_DIR="$(pwd)"
+MACHINE=$1
+PID=$2
+
+# error handling
+error_handler() {
+  xzcat $WORK_DIR/img/uninstall_fail_error.img.xz > /dev/fb0
+  sync
+  # always exit without error, to prevent immediate boot of flashforge software
+  exit 0
+}
+trap '[ $? -eq 0 ] && exit 0 || error_handler' EXIT
+
+do_setup()
+{
+    MOD_INIT_FILE="/etc/init.d/S00klipper_mod"
+    MOD_INIT_LOG_PREFIX="/root/mod_init"
+    MOD_DIR="/data/.klipper_mod"
+
+    # remove everything
+    rm -f "$MOD_INIT_FILE"
+    rm -f "$MOD_INIT_LOG_PREFIX"*
+    rm -rf "$MOD_DIR"
+
+    # update end image
+    xzcat $WORK_DIR/img/uninstall_ok.img.xz > /dev/fb0
+}
+
+# write log (if mount point is found)
+if grep "^/dev/sd[ab12]* /mnt " /proc/mounts; then
+  do_setup &> /mnt/klipper_mod_uninstall.log
+  sync
+else
+  do_setup
+fi
+
+exit 0

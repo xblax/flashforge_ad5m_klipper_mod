651f04de8089043c57afdd7a0374d06eeefa2c62
xblax <xblax@incy.net>
2024-03-19 22:02:18 +0100

    build_overlay: split variant-lite and variant-klipperscreen from common

--- build_overlay/common/etc/init/init_chroot.sh
+++ build_overlay/common/etc/init/init_chroot.sh
@@ -8,8 +8,9 @@ mkdir -p /run/lock/subsys
 insmod /lib/modules/cbc.ko
 insmod /lib/modules/md4.ko
 insmod /lib/modules/sha512_generic.ko
-insmod /lib/modules/uinput.ko
 insmod /mnt/orig_root/lib/modules/8821cu.ko
+# load kernel modules for touch
+insmod /lib/modules/uinput.ko 2> /dev/null
 
 # mount swap
 swapon /mnt/swap

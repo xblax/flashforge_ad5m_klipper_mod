e5cafd169fac506804269e57227c9350d743ff2a
xblax <xblax@incy.net>
2024-04-15 22:40:42 +0200

    buildroot: add ff-ad5m-audio and ff-ad5m-backlight packages by @consp to external tree
    Also mido package which is a dependency to ff-ad5m-audio.

--- build_scripts/buildroot/external.mk
+++ build_scripts/buildroot/external.mk
@@ -1,3 +1,5 @@
 # Disable ccache compiler_check, since it messes with with ccache if the SDK was rebuild
 # ~/.buildroot-ccache must be manually deleted if any toolchain settings change
 export CCACHE_COMPILERCHECK=none
+
+include $(sort $(wildcard $(BR2_EXTERNAL_AD5M_PATH)/package/*/*.mk))

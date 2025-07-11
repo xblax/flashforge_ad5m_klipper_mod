b26aa91351b4bdfd08b256f80027dac528a56a14
minicx <minicx@disroot.org>
2025-07-11 16:59:57 +0300

    fluidd: add package for Fluidd web UI
    Add a Buildroot package for Fluidd (v1.34.2).
    
    Signed-off-by: minicx <minicx@disroot.org>

---
+++ build_scripts/buildroot/package/fluidd/fluidd.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# fluidd
+#
+################################################################################
+
+FLUIDD_VERSION = v1.34.2
+FLUIDD_SITE = https://github.com/fluidd-core/fluidd/releases/download/$(FLUIDD_VERSION)
+FLUIDD_SOURCE = fluidd.zip
+
+define FLUIDD_EXTRACT_CMDS 
+	$(UNZIP) $(FLUIDD_DL_DIR)/$(FLUIDD_SOURCE) -d $(@D)/
+endef
+
+define FLUIDD_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/opt/fluidd
+	cp -a $(@D)/* $(TARGET_DIR)/opt/fluidd/
+endef
+
+$(eval $(generic-package))
+

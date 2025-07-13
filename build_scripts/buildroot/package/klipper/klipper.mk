ce47c94530266c0c4c89471f07c078cc525b1dc2
minicx <minicx@disroot.org>
2025-07-13 03:27:24 +0300

    feat(klipper): Integrate MCU firmware compilation into Buildroot
    Signed-off-by: minicx <minicx@disroot.org>

--- build_scripts/buildroot/package/klipper/klipper.mk
+++ build_scripts/buildroot/package/klipper/klipper.mk
@@ -22,6 +22,11 @@ KLIPPER_DEPENDENCIES = \
 	python-setuptools \
 	python-msgspec
 
+ifneq ($(BR2_PACKAGE_KLIPPER_CONFIGS),)
+KLIPPER_DEPENDENCIES += host-arm-gnu-toolchain host-python3
+KLIPPER_MCU_CONFIGS_LIST = $(call qstrip,$(BR2_PACKAGE_KLIPPER_CONFIGS))
+endif
+
 define KLIPPER_BUILD_C_HELPER
 	$(RM) -rf $(@D)/klippy/chelper/c_helper.so
 	$(TARGET_MAKE_ENV)
@@ -44,7 +49,31 @@ define KLIPPER_INSTALL_TARGET_CMDS
       > $(TARGET_DIR)/opt/klipper/klippy/.version
 endef
 
+define KLIPPER_BUILD_MCU_FIRMWARES
+	$(if $(KLIPPER_MCU_CONFIGS_LIST),
+		$(foreach config,$(KLIPPER_MCU_CONFIGS_LIST),
+			$(call KLIPPER_BUILD_MCU_FIRMWARE,$(config))
+		)
+	)
+endef
+
+define KLIPPER_BUILD_MCU_FIRMWARE
+	rm -rf $(@D)/out
+	cp $(1) $(@D)/.config
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) \
+		CROSS_PREFIX=$(HOST_DIR)/bin/arm-none-eabi- \
+		olddefconfig
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) \
+		CROSS_PREFIX=$(HOST_DIR)/bin/arm-none-eabi-
+	mkdir -p $(TARGET_DIR)/opt/klipper/firmware
+	cp -f $(@D)/out/klipper.bin $(TARGET_DIR)/opt/klipper/firmware/$(notdir $(basename $(1))).bin
+	cp -f $(@D)/out/klipper.elf $(TARGET_DIR)/opt/klipper/firmware/$(notdir $(basename $(1))).elf
+endef
 
 KLIPPER_POST_BUILD_HOOKS += KLIPPER_BUILD_C_HELPER
+
+ifneq ($(KLIPPER_MCU_CONFIGS_LIST),)
+KLIPPER_POST_BUILD_HOOKS += KLIPPER_BUILD_MCU_FIRMWARES
+endif
 
 $(eval $(generic-package))

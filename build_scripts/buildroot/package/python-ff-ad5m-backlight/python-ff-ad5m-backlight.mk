f17574cd3999208eeea8c27499980d1735a67610
minicx <minicx@disroot.org>
2025-08-23 22:58:47 +0300

    python-ff-ad5m-backlight: Switch package source to fork
    Signed-off-by: minicx <minicx@disroot.org>

--- build_scripts/buildroot/package/python-ff-ad5m-backlight/python-ff-ad5m-backlight.mk
+++ build_scripts/buildroot/package/python-ff-ad5m-backlight/python-ff-ad5m-backlight.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FF_AD5M_BACKLIGHT_VERSION = 0.1
-PYTHON_FF_AD5M_BACKLIGHT_SOURCE = ff-ad5m-backlight-$(PYTHON_FF_AD5M_BACKLIGHT_VERSION).tar.gz
-PYTHON_FF_AD5M_BACKLIGHT_SITE = https://files.pythonhosted.org/packages/80/aa/c90577ff17f49daa7ddabf2c35829d81f6f6b391c79518ff06dfbd404637
+PYTHON_FF_AD5M_BACKLIGHT_VERSION = 3cd9a0898e51a18e83976c7cab29037f8364f406
+PYTHON_FF_AD5M_BACKLIGHT_SITE = https://github.com/loss-and-quick/flashforge_ad5m_backlight.git
+PYTHON_FF_AD5M_BACKLIGHT_SITE_METHOD = git
 PYTHON_FF_AD5M_BACKLIGHT_SETUP_TYPE = setuptools
 PYTHON_FF_AD5M_BACKLIGHT_LICENSE = GPLv3
 PYTHON_FF_AD5M_BACKLIGHT_LICENSE_FILES = LICENSE
